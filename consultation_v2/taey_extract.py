87b4738038e15122129057c88a6d44bfe940ddc3
Jesse LaRose <jesse@taey.ai>
2026-07-30 17:56:54 +0000

    Observe transient completion controls after send
    Closes task-9f27db5f

--- consultation_v2/taey_extract.py
+++ consultation_v2/taey_extract.py
@@ -2781,11 +2781,19 @@ class TaeyConsultExtractionSeat:
             elif name == 'post_submit':
                 self.post_submitted = True
             if name in {'submit', 'post_submit'}:
-                completion_control = self._find_semantic_control(
-                    'completion',
-                    must_show=False,
-                    scroll=False,
-                )
+                completion_probe_started = time.monotonic()
+                completion_control = None
+                while (
+                    completion_control is None
+                    and time.monotonic() - completion_probe_started < 10.0
+                ):
+                    completion_control = self._find_semantic_control(
+                        'completion',
+                        must_show=False,
+                        scroll=False,
+                    )
+                    if completion_control is None:
+                        time.sleep(0.25)
                 serialized_completion = self._serializable_found(
                     completion_control
                 )
@@ -2793,6 +2801,10 @@ class TaeyConsultExtractionSeat:
                     'semantic_step': name,
                     'stop_seen_after_action': completion_control is not None,
                     'completion_control_after_action': serialized_completion,
+                    'completion_probe_elapsed_seconds': round(
+                        time.monotonic() - completion_probe_started,
+                        3,
+                    ),
                 }
                 action_evidence.update(self.completion_evidence)
             return {
